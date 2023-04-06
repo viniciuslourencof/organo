a4666de3eabb4c384848f60695a32cf858d61053
Vinicius Lourenço Ferreira <lourencovinicius00@gmail.com>
2023-04-06 06:54:48 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="O bnanner principal da página do Organo"></img>
+        </header>
+    )
+}
+
+export default Banner
